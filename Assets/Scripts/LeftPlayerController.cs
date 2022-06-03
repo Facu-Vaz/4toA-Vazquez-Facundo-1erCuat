8c2598d4d7544796d74e825dbcc06323800d6a4c
Facu-Vaz <facundo.vazber@gmail.com>
2022-06-03 19:51:23 -0300

    Controles de ambos jugadores

---
+++ Assets/Scripts/LeftPlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftPlayerController : MonoBehaviour
+{
+    public float movSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W) && transform.position.z < 6.75)
+        {
+            transform.Translate(0, 0, movSpeed);
+        }
+        if (Input.GetKey(KeyCode.S) && transform.position.z > -6.75)
+        {
+            transform.Translate(0, 0, -movSpeed);
+        }
+    }
+}
---
+++ Assets/Scripts/RigthPlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigthPlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    public float movSpeed;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.UpArrow) && transform.position.z < 6.75)
+        {
+            transform.Translate(0, 0, movSpeed);
+        }
+        if (Input.GetKey(KeyCode.DownArrow) && transform.position.z > -6.75)
+        {
+            transform.Translate(0, 0, -movSpeed);
+        }
+    }
+}
